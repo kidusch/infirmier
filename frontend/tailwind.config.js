953e37e3aa93daa6798430d9b22379a00ebc034a
JC BUISSON <buisson@enseeiht.fr>
2024-04-19 11:14:01 +0200

    WIP

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
       "./src/**/*.{vue,js,ts,jsx,tsx}",
    ],
    theme: {
-      extend: {},
+      container: {
+         center: true,
+         padding: '2rem',
+      },
+      extend: {
+         colors: {
+            primary: '#0B92D2',
+            secondary: '#C80404',
+            accent: '#EBF9FF',
+            'accent-darker': '#C7EBFC',
+         }
+      },
    },
    plugins: [require("daisyui")],
 }
