a901faeda13a29273143e063779d30892cac0bda
JC BUISSON <buisson@enseeiht.fr>
2024-04-20 07:31:18 +0200

    WIP

---
+++ frontend/src/views/auth/Signin-TO-DELETE.vue
@@ -0,0 +1,34 @@
+<template>
+   
+   <h1>Se connecter au Journal de Bord Infirmier</h1>
+
+   <div>
+      <button class="btn btn-wide" @click="goGoogle">
+         <img src="/src/assets/google_logo.svg" class="w-6"/>
+         Continuer avec Google
+      </button>
+   </div>
+
+   <div>
+      <button class="btn btn-wide" @click="localSignin">
+         Continuer avec un email @
+      </button>
+   </div>
+
+   <div>
+      Pas encore de compte ? <RouterLink to="/signup" class="link">S'inscrire</RouterLink>
+   </div>
+
+</template>
+
+<script setup>
+import router from "/src/router"
+
+const goGoogle = () => {
+   window.location.href = `/auth/google?cnxid=${sessionStorage.cnxid}`
+}
+
+const localSignin = () => {
+   router.push('/local-signin')
+}
+</script>
