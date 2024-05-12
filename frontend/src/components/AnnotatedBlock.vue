aa8f4059d99c1f322202dc7123a4a78f2f75df1b
JC BUISSON <buisson@enseeiht.fr>
2024-05-12 08:13:53 +0200

    WIP

--- frontend/src/components/AnnotatedBlock.vue
+++ frontend/src/components/AnnotatedBlock.vue
@@ -10,7 +10,7 @@
 
             'bg-yellow-200': part.highlight === 'yellow', 'bg-orange-200': part.highlight === 'orange', 'bg-purple-200': part.highlight === 'purple',
 
-            'inline-block': isSpan || isBoldSpan
+            'inline': isSpan || isBoldSpan
          }"
          @mouseup="onMouseUp(part)" @mousedown="onMouseDown(part)" v-html="part.text"
          @touchend="onMouseUp(part)" @touchstart="onMouseDown(part)"
@@ -64,101 +64,101 @@ const isLexicon = computed(() => type.value === 'lexicon')
 let selectedPart
 
 function onMouseUp(part) {
-   // if (!props.highlight) return
-   // if (selectedPart !== part) { console.log('outside', part); return; }
+   if (!props.highlight) return
+   if (selectedPart !== part) { console.log('outside', part); return; }
 
-   // const selection = window.getSelection()
-   // // selection must not be empty
-   // if (!selection || selection.rangeCount === 0) return
-   // const range = selection.getRangeAt(0)
-   // if (range.startOffset === range.endOffset) return
+   const selection = window.getSelection()
+   // selection must not be empty
+   if (!selection || selection.rangeCount === 0) return
+   const range = selection.getRangeAt(0)
+   if (range.startOffset === range.endOffset) return
 
-   // console.log('part', part)
-   // console.log('range', range)
-   // const partIndex = parts.value.indexOf(part)
-   // if (range.startOffset === part.start) {
-   //    // replace part by 2 sub-parts, the left one being highlighted
-   //    parts.value.splice(partIndex, 1, ...[
-   //       {
-   //          text: part.text.substring(range.startOffset, range.endOffset),
-   //          start: part.start + range.startOffset,
-   //          end: part.start + range.endOffset,
-   //          highlight: props.highlight,
-   //       },
-   //       {
-   //          text: part.text.substring(range.endOffset, part.end),
-   //          start: part.start + range.endOffset,
-   //          end: part.end,
-   //       },
-   //    ])
-   // } else if (range.endOffset === part.end) {
-   //    // replace part by 2 sub-parts, the right one being highlighted
-   //    parts.value.splice(partIndex, 1, ...[
-   //       {
-   //          text: part.text.substring(0, range.startOffset),
-   //          start: 0,
-   //          end: part.start + range.startOffset,
-   //       },
-   //       {
-   //          text: part.text.substring(range.startOffset, range.endOffset),
-   //          start: part.start + range.startOffset,
-   //          end: part.end,
-   //          highlight: props.highlight,
-   //       },
-   //    ])
-   // } else {
-   //    // replace part by 3 sub-parts, the middle one being highlighted
-   //    parts.value.splice(partIndex, 1, ...[
-   //       {
-   //          text: part.text.substring(0, range.startOffset),
-   //          start: part.start,
-   //          end: part.start + range.startOffset,
-   //       },
-   //       {
-   //          text: part.text.substring(range.startOffset, range.endOffset),
-   //          start: part.start + range.startOffset,
-   //          end: part.start + range.endOffset,
-   //          highlight: props.highlight,
-   //       },
-   //       {
-   //          text: part.text.substring(range.endOffset, part.end),
-   //          start: part.start + range.endOffset,
-   //          end: part.end,
-   //       },
-   //    ])
-   // }
+   console.log('part', part)
+   console.log('range', range)
+   const partIndex = parts.value.indexOf(part)
+   if (range.startOffset === part.start) {
+      // replace part by 2 sub-parts, the left one being highlighted
+      parts.value.splice(partIndex, 1, ...[
+         {
+            text: part.text.substring(range.startOffset, range.endOffset),
+            start: part.start + range.startOffset,
+            end: part.start + range.endOffset,
+            highlight: props.highlight,
+         },
+         {
+            text: part.text.substring(range.endOffset, part.end),
+            start: part.start + range.endOffset,
+            end: part.end,
+         },
+      ])
+   } else if (range.endOffset === part.end) {
+      // replace part by 2 sub-parts, the right one being highlighted
+      parts.value.splice(partIndex, 1, ...[
+         {
+            text: part.text.substring(0, range.startOffset),
+            start: 0,
+            end: part.start + range.startOffset,
+         },
+         {
+            text: part.text.substring(range.startOffset, range.endOffset),
+            start: part.start + range.startOffset,
+            end: part.end,
+            highlight: props.highlight,
+         },
+      ])
+   } else {
+      // replace part by 3 sub-parts, the middle one being highlighted
+      parts.value.splice(partIndex, 1, ...[
+         {
+            text: part.text.substring(0, range.startOffset),
+            start: part.start,
+            end: part.start + range.startOffset,
+         },
+         {
+            text: part.text.substring(range.startOffset, range.endOffset),
+            start: part.start + range.startOffset,
+            end: part.start + range.endOffset,
+            highlight: props.highlight,
+         },
+         {
+            text: part.text.substring(range.endOffset, part.end),
+            start: part.start + range.endOffset,
+            end: part.end,
+         },
+      ])
+   }
 }
 
 function onMouseDown(part) {
-   // selectedPart = part
+   selectedPart = part
 
-   // if (props.highlight === 'eraser' && part.highlight) {
-   //    console.log('erase', part)
-   //    selectedPart = null
-   //    const partIndex = parts.value.indexOf(part)
-   //    if (partIndex === 0) {
-   //       const after = parts.value[partIndex + 1]
-   //       parts.value.splice(partIndex, 2, {
-   //          text: part.text + after.text,
-   //          start: part.start,
-   //          end: after.end,
-   //       })
-   //    } else if (partIndex === parts.value.length - 1) {
-   //       const before = parts.value[partIndex - 1]
-   //       parts.value.splice(partIndex - 1, 2, {
-   //          text: before.text + part.text,
-   //          start: before.start,
-   //          end: part.end,
-   //       })
-   //    } else {
-   //       const before = parts.value[partIndex - 1]
-   //       const after = parts.value[partIndex + 1]
-   //       parts.value.splice(partIndex - 1, 3, {
-   //          text: before.text + part.text + after.text,
-   //          start: before.start,
-   //          end: after.end,
-   //       })
-   //    }
-   // }
+   if (props.highlight === 'eraser' && part.highlight) {
+      console.log('erase', part)
+      selectedPart = null
+      const partIndex = parts.value.indexOf(part)
+      if (partIndex === 0) {
+         const after = parts.value[partIndex + 1]
+         parts.value.splice(partIndex, 2, {
+            text: part.text + after.text,
+            start: part.start,
+            end: after.end,
+         })
+      } else if (partIndex === parts.value.length - 1) {
+         const before = parts.value[partIndex - 1]
+         parts.value.splice(partIndex - 1, 2, {
+            text: before.text + part.text,
+            start: before.start,
+            end: part.end,
+         })
+      } else {
+         const before = parts.value[partIndex - 1]
+         const after = parts.value[partIndex + 1]
+         parts.value.splice(partIndex - 1, 3, {
+            text: before.text + part.text + after.text,
+            start: before.start,
+            end: after.end,
+         })
+      }
+   }
 }
 </script>
