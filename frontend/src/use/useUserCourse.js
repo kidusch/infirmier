d77a6c3a63eaccb0cad65650dd40e858c8ca225f
JC BUISSON <buisson@enseeiht.fr>
2024-06-05 06:37:48 +0100

    WIP

--- frontend/src/use/useUserCourse.js
+++ frontend/src/use/useUserCourse.js
@@ -7,7 +7,7 @@ import app from '/src/client-app.js'
 // state backed in SessionStorage
 const initialState = () => ({
    userCourseCache: {},
-   theUserCourseReady: {},
+   theUserCourseStatus: {},
 })
  
 const userCourseState = useSessionStorage('user-course-state', initialState())
@@ -23,8 +23,11 @@ app.service('user_course').on('create', (userCourse) => {
 
 // get or create the unique user_course associated to (user_id, course_id)
 export const getTheUserCourse = async (user_id, course_id) => {
-   const isReady = userCourseState.value.theUserCourseReady[user_id + ':' + course_id]
-   if (isReady) return Object.values(userCourseState.value.userCourseCache).find(userCourse => userCourse.user_id === user_id && userCourse.course_id === course_id)
+   const key = user_id + ':' + course_id
+   if (userCourseState.value.theUserCourseStatus[key] === 'ready') {
+      return Object.values(userCourseState.value.userCourseCache).find(userCourse => userCourse.user_id === user_id && userCourse.course_id === course_id)
+   }
+   userCourseState.value.theUserCourseStatus[key] = 'ongoing'
    let [userCourse] = await app.service('user_course').findMany({
       where: { user_id, course_id },
    })
@@ -34,28 +37,33 @@ export const getTheUserCourse = async (user_id, course_id) => {
       })
    }
    userCourseState.value.userCourseCache[userCourse.id] = userCourse
-   userCourseState.value.theUserCourseReady[user_id + ':' + course_id] = true
+   userCourseState.value.theUserCourseStatus[key] = 'ready'
    return userCourse
 }
 
 export const theUserCourse = computed(() => (user_id, course_id) => {
-   const isReady = userCourseState.value.theUserCourseReady[user_id + ':' + course_id]
-   if (isReady) return Object.values(userCourseState.value.userCourseCache).find(userCourse => userCourse.user_id === user_id && userCourse.course_id === course_id)
-   app.service('user_course').findMany({
-      where: { user_id, course_id },
-   }).then((userCourses) => {
-      if (userCourses.length === 0) {
-         app.service('user_course').create({
-            data: { user_id, course_id },
-         }).then(userCourse => {
-            userCourseState.value.userCourseCache[userCourse.id] = userCourse
-            userCourseState.value.theUserCourseReady[user_id + ':' + course_id] = true
-         })
-      } else {
-         userCourseState.value.userCourseCache[userCourses[0].id] = userCourses[0]
-         userCourseState.value.theUserCourseReady[user_id + ':' + course_id] = true
-      }
-   })
+   const key = user_id + ':' + course_id
+   if (userCourseState.value.theUserCourseStatus[key] === 'ready') {
+      return Object.values(userCourseState.value.userCourseCache).find(userCourse => userCourse.user_id === user_id && userCourse.course_id === course_id)
+   }
+   if (userCourseState.value.theUserCourseStatus[key] !== 'ongoing') {
+      userCourseState.value.theUserCourseStatus[key] = 'ongoing'
+      app.service('user_course').findMany({
+         where: { user_id, course_id },
+      }).then((userCourses) => {
+         if (userCourses.length === 0) {
+            app.service('user_course').create({
+               data: { user_id, course_id },
+            }).then(userCourse => {
+               userCourseState.value.userCourseCache[userCourse.id] = userCourse
+               userCourseState.value.theUserCourseStatus[key] = 'ready'
+            })
+         } else {
+            userCourseState.value.userCourseCache[userCourses[0].id] = userCourses[0]
+            userCourseState.value.theUserCourseStatus[key] = 'ready'
+         }
+      })
+   }
 })
 
 export const updateUserCourse = async (id, data) => {
