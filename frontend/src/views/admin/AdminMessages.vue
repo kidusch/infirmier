5bc467940710f5333e7b930eaefa9b78fb989903
JC BUISSON <buisson@enseeiht.fr>
2024-07-07 14:55:44 +0200

    WIP

--- frontend/src/views/admin/AdminMessages.vue
+++ frontend/src/views/admin/AdminMessages.vue
@@ -19,7 +19,8 @@
                   <div class="flex items-center justify-start gap-3">
                      <!-- circle red/white -->
                      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                        <path :fill="false ? 'red' : 'white'" d="M12.003 21q-1.866 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709"/>
+                        <path :fill="unreadMessagesCountOfStudent(user.id) > 0 ? 'red' : 'white'"
+                           d="M12.003 21q-1.866 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709"/>
                      </svg>
 
                      <div>
@@ -39,7 +40,10 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
+
 import { listOfUser } from '/src/use/useUser'
+import { unreadMessagesCountOfUser2ByUser1 } from '/src/use/useMessage'
 import router from "/src/router"
 
 const props = defineProps({
@@ -53,4 +57,5 @@ const select = (user) => {
    router.push(`/home/${props.userid}/admin-messages-student/${user.id}`)
 }
 
+const unreadMessagesCountOfStudent = computed(() => (studentId) => unreadMessagesCountOfUser2ByUser1.value(props.userid, studentId))
 </script>
--- frontend/src/views/student/Lexicon.vue
+++ frontend/src/views/student/Lexicon.vue
@@ -9,19 +9,6 @@
       </header>
 
       <header class="w-16">
-         <!-- <div class="form-control">
-            <label class="label cursor-pointer">
-               <span class="label-text"><img class="h-6 cursor-pointer" src="/src/assets/flag_fr.svg"></span>
-               <input type="radio" name="radio-10" class="radio checked:bg-blue-400" :checked="lang === 'fr'" @change="lang = 'fr'" />
-            </label>
-         </div>
-         <div class="form-control">
-            <label class="label cursor-pointer">
-               <span class="label-text"><img class="h-6 cursor-pointer" src="/src/assets/flag_en.svg"></span>
-               <input type="radio" name="radio-10" class="radio checked:bg-blue-400" :checked="lang === 'en'" @change="lang = 'en'" />
-            </label>
-         </div> -->
-
          <div class="form-control w-36">
             <label class="label cursor-pointer">
                <span class="label-text"><img class="h-6 cursor-pointer" src="/src/assets/flag_fr.svg"></span>
@@ -31,17 +18,32 @@
          </div>
       </header>
 
+      <!-- Search input -->
+      <label class="input input-bordered flex items-center gap-2 my-2">
+         <input v-model="searchTerms" type="text" class="grow" placeholder="Rechercher..." />
+         <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-6 w-6 opacity-70">
+            <path
+               fill-rule="evenodd"
+               d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+               clip-rule="evenodd" />
+         </svg>
+      </label>
+
       <!-- Main content -->
       <main class="flex flex-col gap-6 pb-6">
          
          <div class="bg-accent p-5 gap-3 flex flex-col rounded-3xl">
 
-            <template v-for="lexicon in lexiconList">
+            <template v-for="lexicon in lexiconList(lang)">
                <div class="collapse" @click="checkedId = lexicon.id">
                   <input type="radio" :checked="lexicon?.id === checkedId" />
-                  <div class="collapse-title text-xl font-medium">{{ lexiconWord(lexicon) }}</div>
+                  <div class="collapse-title text-xl font-medium">{{ lexiconWord(lexicon, lang) }}</div>
                   <div class="collapse-content">
-                     <p>{{ lexiconDesc(lexicon) }}</p>
+                     <p>{{ lexiconDesc(lexicon, lang) }}</p>
                   </div>
                </div>
             </template>
@@ -65,15 +67,26 @@ const props = defineProps({
 
 const checkedId = ref()
 const lang = ref('fr')
+const searchTerms = ref('')
 
-const lexiconList = computed(() => {
-   if (lang.value === 'fr') {
-      return listOfLexicon.value.sort((l1, l2) => l1.french_word > l2.french_word ? 1 : l1.french_word < l2.french_word ? -1 : 0)
-   } else {
-      return listOfLexicon.value.sort((l1, l2) => l1.english_word > l2.english_word ? 1 : l1.english_word < l2.english_word ? -1 : 0)
-   }
+const lexiconList = computed(() => (lang) => {
+   const unfilteredList = lang === 'fr' ?
+      listOfLexicon.value.sort((l1, l2) => l1.french_word > l2.french_word ? 1 : l1.french_word < l2.french_word ? -1 : 0) :
+      listOfLexicon.value.sort((l1, l2) => l1.english_word > l2.english_word ? 1 : l1.english_word < l2.english_word ? -1 : 0)
+   return unfilteredList.filter(lexicon => {
+      if (searchTerms.value.trim().length === 0) return true
+      const terms = searchTerms.value.trim().toLowerCase().split(' ')
+      if (lang === 'fr') {
+         if (terms.some(term => lexicon.french_word.toLowerCase().includes(term))) return true
+         if (terms.some(term => lexicon.french_desc.toLowerCase().includes(term))) return true
+      } else {
+         if (terms.some(term => lexicon.english_word.toLowerCase().includes(term))) return true
+         if (terms.some(term => lexicon.english_desc.toLowerCase().includes(term))) return true
+      }
+      return false
+   })
 })
 
-const lexiconWord = computed(() => (lexicon) => lang.value === 'fr' ? lexicon.french_word : lexicon.english_word)
-const lexiconDesc = computed(() => (lexicon) => lang.value === 'fr' ? lexicon.french_desc : lexicon.english_desc)
+const lexiconWord = computed(() => (lexicon, lang) => lang === 'fr' ? lexicon.french_word : lexicon.english_word)
+const lexiconDesc = computed(() => (lexicon, lang) => lang === 'fr' ? lexicon.french_desc : lexicon.english_desc)
 </script>
