9b6c4ba5a9a14e8c5847552c17a116be18a03d2d
JC BUISSON <buisson@enseeiht.fr>
2024-08-05 17:43:11 +0200

    WIP

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <template>
 
-   <div v-if="needRefresh" class="fixed bottom-0 right-0 m-4 p-4 bg-green-100 border-solid rounded z-40">
+   <div v-if="needRefresh" class="fixed bottom-0 right-0 m-4 p-4 bg-green-100 border-solid rounded z-50">
       Une nouvelle version est disponible <a href="#" class="text-blue-400 hover:underline ml-2" @click="updateServiceWorker">installer</a>
    </div>
 
    <router-view></router-view>
 
-   <Spinner v-if="appState?.spinnerWaitingText">
+   <Spinner v-if="appState?.spinnerWaitingText" class="z-40">
       <div>
          <template v-for="line in appState.spinnerWaitingText">
             <div class="text-center font-bold text-white text-2xl">{{ line }}</div>
