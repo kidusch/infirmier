cd8acbd3a592982a9b0679c5aa97a3b2c4712cf1
JC BUISSON <buisson@enseeiht.fr>
2024-09-16 11:33:54 +0200

    WIP

--- frontend/src/views/FrontPage.vue
+++ frontend/src/views/FrontPage.vue
@@ -67,10 +67,10 @@ onMounted(async () => {
 
    await InAppPurchase.echo({ value: "Hello, Capacitor!" })
 
-   await InAppPurchase.test({ value: "Hello, World!" })
-
    const x = await InAppPurchase.fetchProducts({ value: "premium" })
    console.log('x', x)
+
+   await InAppPurchase.test({ value: "Hello, World!" })
 })
 
 function login() {
